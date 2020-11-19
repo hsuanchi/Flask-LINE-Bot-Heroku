da32cc755cd071b00ecafed51a379a6abe513268
hsuanchi <a0025071@gmail.com>
2020-11-19 15:51:07 +0800

    feat(project): init
    feat(project): init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Flask-LINE-Bot-Heroku
+
+<a href="https://heroku.com/deploy?template=https://github.com/hsuanchi/Flask-LINE-Bot-Heroku">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
+</a>
---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from datetime import datetime
+
+from flask import Flask, abort, request
+
+# https://github.com/line/line-bot-sdk-python
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+
+@app.route("/", methods=["GET", "POST"])
+def callback():
+
+    if request.method == "GET":
+        return "Hello Heroku"
+        
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
+
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+
+        return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    get_message = event.message.text
+
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
