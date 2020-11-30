0f86664f3a498d4b8b2c28d53b6637d9cdd115f0
hsuanchi <a0025071@gmail.com>
2020-11-30 11:41:08 +0800

    docs(img): add img for reademe.md

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
+
 @app.route("/", methods=["GET", "POST"])
 def callback():
 
     if request.method == "GET":
         return "Hello Heroku"
-        
     if request.method == "POST":
         signature = request.headers["X-Line-Signature"]
         body = request.get_data(as_text=True)
@@ -29,6 +29,7 @@ def callback():
             abort(400)
 
         return "OK"
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
